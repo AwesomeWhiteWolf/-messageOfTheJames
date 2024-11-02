18cd650b6c4549deaf35e4e68e4a34093b6d6308
Egor <148298503+AwesomeWhiteWolf@users.noreply.github.com>
2024-11-03 00:56:35 +0800

    Add files via upload

--- test/script.js
+++ test/script.js
@@ -1,21 +1,38 @@
+let correctCount = 0;
+const questionCount = document.querySelectorAll('.answer-submit').length;
 document.querySelectorAll('.answer-submit').forEach(button => {  
   button.addEventListener('click', function() {  
-      const liElement = button.closest('li');  
-      const selectedAnswer = liElement.querySelector('input[type="radio"]:checked');  
-      const correctAnswer = button.getAttribute('data-correct');   
-      if (selectedAnswer) {  
-          const selectedValue = selectedAnswer.value;  
-          if (selectedValue === correctAnswer) {  
-            liElement.querySelector('.accepted').style.display = 'block';    
-            liElement.querySelector('.comment').style.display = 'block';  
-            liElement.querySelector('.correct').style.display = 'none';
-          } else {  
-            liElement.querySelector('.correct').style.display = 'block'; 
-            liElement.querySelector('.comment').style.display = 'block'; 
-            liElement.querySelector('.accepted').style.display = 'none';      
-          }  
-      } else {  
-          alert("Пожалуйста, выберите ответ");  
-      }  
+    const liElement = button.closest('li');  
+    if (liElement.classList.contains('disabled')) {  
+      return; 
+    }  
+    const selectedAnswer = liElement.querySelector('input[type="radio"]:checked');  
+    const correctAnswer = button.getAttribute('data-correct');   
+    if (selectedAnswer) {  
+        const selectedValue = selectedAnswer.value;  
+        if (selectedValue === correctAnswer) {  
+          liElement.querySelector('.accepted').style.display = 'block';    
+          liElement.querySelector('.comment').style.display = 'block';  
+          liElement.querySelector('.correct').style.display = 'none';
+          correctCount++;
+        } else {  
+          liElement.querySelector('.correct').style.display = 'block'; 
+          liElement.querySelector('.comment').style.display = 'block'; 
+          liElement.querySelector('.accepted').style.display = 'none';
+        }  
+    } else {  
+        alert("Пожалуйста, выберите ответ");  
+    } 
+    liElement.classList.add('disabled');   
   });  
 });  
+
+const resultsText = document.querySelector('.resultsText');
+function results() {
+  if (correctCount === questionCount) {
+    resultsText.innerHTML = `Поздравляю, вы ответили правильно на все ${correctCount} вопросов`;
+  }
+  else {
+    resultsText.innerHTML = `Правильных ответов: ${correctCount}`;
+  }
+}
